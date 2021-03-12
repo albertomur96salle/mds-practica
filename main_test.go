09c3752e00b3d15db3f6817b97f63cfbef31540e
Alberto Mur Rodrigo <alberto.mur@students.salle.url.edu>
2021-03-12 19:50:58 +0100

    Created test file

--- main_test.go
+++ main_test.go
@@ -1,17 +1,41 @@
-//package mainimport
-//
-//import (
-//	"log"
-//	"net/http"
-//)
-//
-//type Server struct{}
-//
-//func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-//	w.WriteHeader(http.StatusOK)
-//	w.Header().Set("Content-Type", "application/json")
-//	w.Write([]byte(`{"message": "hello world"}`))
-//}
-//func main() { s := &Server{}
-//	http.Handle("/", s)
-//	log.Fatal(http.ListenAndServe(":8080", nil)) }
+package mainimport
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHelloWorld(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatalf("could not created request: %v", err)
+	}
+	server := &Server{}
+	t.Run("return Paco's score", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		request := createNewRequest()
+		server.ServeHTTP(response, request)
+
+		assertStatus(response.Code, http.StatusOK, t)
+		assertResponseBody(t, "{\"message\": \"hello world\"}", response.Body.String())
+
+	})
+}
+
+func createNewRequest() *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+	return request
+}
+
+func assertStatus(result int, expected int, t *testing.T) {
+	if result != expected {
+		t.Errorf("wrong status status result '%d', status expected '%d'", result, expected)
+	}
+}
+
+func assertResponseBody(t *testing.T, expected string, result string) {
+	if result != expected {
+		t.Errorf("result '%s', expected '%s'", result, expected)
+	}
+}
